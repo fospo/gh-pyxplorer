16eb05bc2b9e9ea191b667debb76ee0022e2466f
libremente <surf@libremente.eu>
2023-02-09 13:25:37 +0100

    chore: add tests

---
+++ tests.py
@@ -0,0 +1,54 @@
+import unittest
+from common import explore_licenses, group_by_project
+from github import Github, Repository
+
+
+# write test for group_by_project function
+class TestGroupByProject(unittest.TestCase):
+    def test_none(self):
+        lines = None
+        expected = {}
+        self.assertEqual(group_by_project(lines), expected)
+
+    def test_empty_list(self):
+        lines = []
+        expected = {}
+        self.assertEqual(group_by_project(lines), expected)
+
+    def test_group_by_project(self):
+        lines = ["0, prova/prova1-etc", "1, prova/prova1-bbb", "2, prova/prova3-etc"]
+        expected = {"prova1": ["prova1-etc", "prova1-bbb"], "prova3": ["prova3-etc"]}
+        self.assertEqual(group_by_project(lines), expected)
+
+    def test_group_by_project_with_empty_line(self):
+        lines = [
+            "0, prova/prova1-etc",
+            "1, prova/prova1-bbb",
+            "2, prova/prova3-etc",
+            "",
+        ]
+        expected = {"prova1": ["prova1-etc", "prova1-bbb"], "prova3": ["prova3-etc"]}
+        self.assertEqual(group_by_project(lines), expected)
+
+    def test_group_by_project_with_empty_line_and_spaces(self):
+        lines = [
+            "0, prova/prova1-etc",
+            "1, prova/prova1-bbb",
+            "2, prova/prova3-etc",
+            "",
+            "",
+        ]
+        expected = {"prova1": ["prova1-etc", "prova1-bbb"], "prova3": ["prova3-etc"]}
+        self.assertEqual(group_by_project(lines), expected)
+
+
+# test for common.py
+class TestCommon(unittest.TestCase):
+    def test_explore_licenses(self):
+        repo = Github().get_repo("pagopa/pagopa-api")
+        expected = "pagopa-api,MIT License"
+        self.assertEqual(explore_licenses(repo), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
